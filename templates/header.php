a9503f21281ab686ed2cb264a36aa63083eb9c06
Filip Van Reeth <filip@Filips-MacBook-Pro.local>
2015-08-02 12:26:16 +0200

    Changes

--- templates/header.php
+++ templates/header.php
@@ -4,32 +4,49 @@
 	<?php get_template_part('templates/eagle/header'); ?>
 
 	<nav id="main-navigation" class="mobile">
-		<div class="menu-icon">
-			<div class="menu">
-				<i class="fa fa-bars"></i>
+		<div class="menu">
+			<div class="menu-icon">
+				<i class="fa fa-bars" id="open-menu-active"></i>
 			</div>
 		</div>
-		<?php {
-			$mnav = array(
-				'theme_location'  => 'mnav',
-				'menu'            => '',
-				'container'       => '',
-				'container_class' => '',
-				'container_id'    => '',
-				'menu_class'      => 'mnav',
-				'menu_id'         => 'mnav-mobile',
-				'echo'            => true,
-				'fallback_cb'     => '',
-				'before'          => '',
-				'after'           => '',
-				'link_before'     => '',
-				'link_after'      => '',
-				'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-				'depth'           => 0,
-				'walker'          => ''
-			);
-			wp_nav_menu( $mnav ); }
-		?>
+		<div class="menu-active" id="menu-active">
+			<div class="menu-icon">
+				<i class="fa fa-close" id="close-menu-active"></i>
+			</div>
+			<?php {
+				$mnav = array(
+					'theme_location'  => 'mnav',
+					'menu'            => '',
+					'container'       => '',
+					'container_class' => '',
+					'container_id'    => '',
+					'menu_class'      => 'mnav',
+					'menu_id'         => 'mnav-mobile',
+					'echo'            => true,
+					'fallback_cb'     => '',
+					'before'          => '',
+					'after'           => '',
+					'link_before'     => '',
+					'link_after'      => '',
+					'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+					'depth'           => 0,
+					'walker'          => ''
+				);
+				wp_nav_menu( $mnav ); }
+			?>
+		</div>
+		<script type="text/javascript">
+				jQuery(function(){
+					jQuery("#open-menu-active").click(function () {
+						//jQuery("#open-menu-active").toggleClass("active");
+						jQuery("#menu-active").slideToggle();
+					});
+					jQuery("#close-menu-active").click(function () {
+						//jQuery("#open-menu-active").toggleClass("active");
+						jQuery("#menu-active").slideToggle();
+					});
+				});
+		</script>
 	</nav>
 
 </header>
